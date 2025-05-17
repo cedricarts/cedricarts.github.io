521799e59394d5812eb87508b4f527667d80dda9
Cedric Arts <152818750+cedricarts@users.noreply.github.com>
2025-05-17 10:11:59 +0200

    Update script.js
    Toggle dark and light mode

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-let username = window.prompt("What is your name");
+// Declaration of variables
+const audio = document.getElementById("audio");
 
-document.getElementById("username").textContent = "Welcome... " + username + ".";
+
+//audio.style.display = "none";
+
+// Ligth - Dark Mode Toggle
+const darkMode = document.getElementById("dark");
+const lightMode = document.getElementById("light");
+const userName = document.getElementById("userName");
+const body = document.body;
+
+darkMode.onclick = toggleDarkMode;
+lightMode.onclick = toggleLightMode;
+lightMode.style.display = "none";
+
+
+function toggleDarkMode(){
+    document.body.style.backgroundColor  = "black";
+    lightMode.style.display = "block";
+    darkMode.style.display = "none";
+    body.style.color = "white";
+    body.style.transition = "background-color 0.5s ease";
+}
+
+function toggleLightMode(){
+    document.body.style.backgroundColor = "white";
+    darkMode.style.display = "block";
+    lightMode.style.display = "none";
+    body.style.color = "black";
+    body.style.transition = "background-color 0.5s ease";
+}
+
